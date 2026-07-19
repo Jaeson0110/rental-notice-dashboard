74aa5c29a43ceb9aa933c785408993606a6758c6
Jaeson0110 <lovebear1997@gmail.com>
2026-07-19 13:48:22 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://bgmwprptrjhtyvzjhstw.supabase.co",
-  supabaseAnonKey: "sb_publishable_ZnOJHU197xZefqJXWWnMAg",
+  supabaseAnonKey: "sb_publishable_ZnOJHU197xZefqJXWWnMAg_2nekjcgu",
   appName: "우리집 임대공고",
   loginRedirectUrl: window.location.origin + window.location.pathname
 };
